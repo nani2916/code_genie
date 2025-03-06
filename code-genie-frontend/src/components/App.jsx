2fc66de854830c0de08ea0d242841ccfe0c1b8b0
nani2916 <rpangulu@kent.edu>
2025-03-06 17:49:54 +0000

    added verify-code frontend route

--- code-genie-frontend/src/components/App.jsx
+++ code-genie-frontend/src/components/App.jsx
@@ -3,7 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./Main";
 import Register from "./Register";
 import Login from "./Login";
+import Home from "./Home";
+import EditProfile from "./EditProfile";
+import ChangePassword from "./ChangePassword";
+import { ToastContainer } from "react-toastify";
+import ChatPage from "./ChatPage";
 import ResetPassword from "./ResetPassword";
+import VerifyCode from "./VerifyCode";
+import NewPassword from "./NewPassword";
 
 function App() {
     return (
@@ -13,6 +20,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/reset-password" element={<ResetPassword/>} />
+          <Route path="/verify-code" element={<VerifyCode />} />
         </Routes>
       <ToastContainer />
     </div>
