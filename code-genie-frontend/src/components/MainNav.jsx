36222d894ca1abbe32498362feb5732c8651499e
nani2916 <rpangulu@kent.edu>
2025-03-06 17:27:52 +0000

    added main nav component

---
+++ code-genie-frontend/src/components/MainNav.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/main-nav.css';
+import logo from '../assets/images/logo.jpg'; 
+
+const MainNav = () => {
+    return (
+        <div>
+          <nav >
+          <Link to="/">
+            <img className="logo" src={logo} alt="CodeGenie Logo" />
+          </Link>
+          <ul>
+            <li><Link to="/" >Home</Link></li>
+            
+            <li>
+              <Link to="/register">
+                Register
+              </Link>
+            </li>
+            <li>
+              <Link to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+    )
+};
+
+export default MainNav;
