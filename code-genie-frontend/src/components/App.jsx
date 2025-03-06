95cab39fad27384e51baaa3589cd37e0c1afdf6b
nani2916 <rpangulu@kent.edu>
2025-03-06 17:46:45 +0000

    added fronten resetpassword route

--- code-genie-frontend/src/components/App.jsx
+++ code-genie-frontend/src/components/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./Main";
 import Register from "./Register";
 import Login from "./Login";
+import ResetPassword from "./ResetPassword";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword/>} />
         </Routes>
       <ToastContainer />
     </div>
